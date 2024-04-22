c8177e326655c6ebaf4a55d55077076f47d82ba8
FedeCaprari <159435636+FedeCaprari@users.noreply.github.com>
2024-04-22 12:18:39 +0200

    Update Ukraine_graph_generator.py

--- Ukraine_graph_generator.py
+++ Ukraine_graph_generator.py
@@ -4,6 +4,8 @@ import plotly.express as px
 from kaggle.api.kaggle_api_extended import KaggleApi
 import os
 import zipfile
+import time
+st.set_page_config(page_title= "Ukraine Missiles Launched vs Intecepted Dashboard", page_icon=":bar_chart:")
 
 def remove_time(data):
     # Ensure that time is removed and only the date is kept
@@ -29,7 +31,8 @@ def aggregate_data(data):
         'destroyed': 'sum'
     }).reset_index()
     # Calculate the daily interception rate
-    daily_data['interception_rate'] = (daily_data['destroyed'] / daily_data['launched'] * 100).fillna(0)
+    daily_data['interception_rate'] = (daily_data['destroyed'] / daily_data['launched'] * 100).fillna(0).round(0).astype(int)
+    daily_data['interception_rate'] = daily_data['interception_rate'].astype(str) + '%'
     return daily_data
 
 def monthly_interception_rate(data):
@@ -37,14 +40,14 @@ def monthly_interception_rate(data):
     data['date'] = pd.to_datetime(data['date'])
     # Group by month and sum the values of 'launched' and 'destroyed'
     monthly_data = data.resample('M', on='date').sum().reset_index()
-    # Calculate the interception rate based on monthly sums
-    monthly_data['interception_rate'] = (monthly_data['destroyed'] / monthly_data['launched'] * 100).fillna(0)
+    # Calculate the interception rate based on monthly sums, round, and convert to string with '%'
+    monthly_data['interception_rate'] = (monthly_data['destroyed'] / monthly_data['launched'] * 100).fillna(0).round(0).astype(int)
+    monthly_data['interception_rate'] = monthly_data['interception_rate'].astype(str) + '%'
     # Format date to show only year and month for readability in the chart
     monthly_data['date'] = monthly_data['date'].dt.strftime('%Y-%m')
     return monthly_data
 
 def plot_data(data):
-    # Plotting the data with wider bars
     fig = px.bar(data, x='date', y=['launched', 'destroyed'],
                  labels={'value': 'Count', 'variable': 'Category'},
                  color_discrete_map={'launched': 'darkblue', 'destroyed': 'darkgray'},
@@ -55,34 +58,38 @@ def plot_data(data):
         xaxis_title='Date',
         yaxis_title='Number of Missiles',
         xaxis=dict(
-            title_font=dict(size=18, color='black'),  # Customize X-axis title font size and color
-            tickfont=dict(size=16, color='black')     # Customize X-axis tick font size and color
+            title_font=dict(size=18, color='black'),
+            tickfont=dict(size=16, color='black'),
+            rangeslider=dict(visible=True),  # Enable the range slider
+            type='date'  # Ensure the x-axis is treated as date
         ),
         yaxis=dict(
-            title_font=dict(size=20, color='black'),  # Customize Y-axis title font size and color
-            tickfont=dict(size=18, color='black'),    # Customize Y-axis tick font size and color
+            title_font=dict(size=20, color='black'),
+            tickfont=dict(size=18, color='black'),
             range=[0, 110]
         )
     )
     return fig
 
 def plot_interception_rate(data):
-    # Plotting the interception rate
     fig = px.line(data, x='date', y='interception_rate', color_discrete_sequence=['darkblue'])
-    fig.update_traces(line=dict(width=4))  # Adjust line width here
-
+    fig.update_traces(line=dict(width=4))
     fig.update_layout(
         title='Monthly Average Interception Rate Over Time',
         xaxis_title='Month',
         yaxis_title='Interception Rate (%)',
         xaxis=dict(
-            title_font=dict(size=18, color='black'),  # Customize X-axis title font size and color
-            tickfont=dict(size=16, color='black'),    # Customize X-axis tick font size and color
-            tickangle=-90, tickmode='linear', dtick='M1'
+            title_font=dict(size=18, color='black'),
+            tickfont=dict(size=16, color='black'),
+            tickangle=-90,
+            tickmode='linear',
+            dtick='M1',
+            rangeslider=dict(visible=True),  # Enable the range slider
+            type='date'  # Ensure the x-axis is treated as date
         ),
         yaxis=dict(
-            title_font=dict(size=20, color='black'),  # Customize Y-axis title font size and color
-            tickfont=dict(size=18, color='black'),    # Customize Y-axis tick font size and color
+            title_font=dict(size=20, color='black'),
+            tickfont=dict(size=18, color='black'),
             range=[50, 100]
         )
     )
@@ -120,17 +127,38 @@ def download_dataset():
     # Clean up the zip file after extraction
     os.remove(zip_path)
 
-    st.success('Data downloaded and extracted successfully!')
-
 # Streamlit app interface
 st.title('Ukraine dashboard')
 st.subheader('Missiles fired, intercepted, and interception rate')
 placeholder = st.empty()
 
-if st.sidebar.button('Get Data'):
-    download_dataset()
-    st.success('Data downloaded successfully!')
+if 'data_loaded' not in st.session_state:
+    st.session_state['data_loaded'] = False
 
+if st.sidebar.button('Get Data', type="primary"):
+
+    download_dataset()
+    st.session_state['data_loaded'] = True
+
+if not st.session_state['data_loaded']:
+    st.markdown("To begin, click the 'GET DATA' button on the left panel")
+if st.session_state['data_loaded']:
+    # This button will allow users to download 'missile_attacks_daily.csv' once it's available
+    success = st.success('Data downloaded and extracted successfully!')
+    time.sleep(3) # Wait for 3 seconds
+    success.empty() # Clear the alert
+
+    st.markdown(
+        "_The database is hosted on Kaggle and updated often. You can find more details [here](https://www.kaggle.com/datasets/piterfm/massive-missile-attacks-on-ukraine)._",
+        unsafe_allow_html=True
+    )    
+    with open("missile_attacks_daily.csv", "rb") as file:
+        btn = st.sidebar.download_button(
+            label="Export Data",
+            data=file,
+            file_name="missile_attacks_daily.csv",
+            mime="text/csv"
+            )
     data = pd.read_csv("missile_attacks_daily.csv")
 
     # Process the dataset
@@ -138,30 +166,25 @@ if st.sidebar.button('Get Data'):
     data_processed = process_dataset(data_processed)  # Process the dataset
     data_aggregated = aggregate_data(data_processed)  # Aggregate the data by day
 
+    latest_date = data_aggregated['date'].max()
+    earliest_date = data_aggregated['date'].min()
+    latest_missiles_fired = int(data_aggregated[data_aggregated['date'] == latest_date]['launched'].sum())
+
+    # Displaying Date Selectors
+    st.sidebar.title("Choose Date Range")
+    start_date = st.sidebar.date_input("Start Date", min_value=earliest_date, max_value=latest_date, value=earliest_date)
+    end_date = st.sidebar.date_input("End Date", min_value=earliest_date, max_value=latest_date, value=latest_date)
+    date_range = start_date,end_date
+
+
+    # Displaying Summary Information
+    st.sidebar.title ("Summary")
+    st.sidebar.markdown(f"* **Information up to:** {latest_date.strftime('%Y-%m-%d')}")
+    st.sidebar.markdown(f"* **Latest number of missiles fired:** {latest_missiles_fired}")
+    st.sidebar.markdown(f"* **Data since:** {earliest_date.strftime('%Y-%m-%d')}")
+
     # Calculate monthly interception rate
     data_monthly_rate = monthly_interception_rate(data_aggregated.copy())  # Aggregate interception rate by month
-
-    # Date slider for filtering
-    min_date, max_date = data_aggregated['date'].min(), data_aggregated['date'].max()
-    start_date = st.sidebar.date_input(
-        "Start Date",
-        value=min_date,
-        min_value=min_date,
-        max_value=max_date,
-        key='start_date',  # Ensuring widget state is tied to session state
-        format="MM-DD-YYYY"
-    )
-
-    end_date = st.sidebar.date_input(
-        "End Date",
-        value=max_date,
-        min_value=min_date,  # Adjust based on desired logic
-        max_value=max_date,
-        key='end_date',  # Ensuring widget state is tied to session state
-        format="MM-DD-YYYY"
-    )
-    
-    date_range = start_date,end_date
 
     # Filter daily data based on selected date range
     filtered_data = data_aggregated[(data_aggregated['date'] >= date_range[0]) & (data_aggregated['date'] <= date_range[1])]
